50bb2aa9fab093ec4dcf5ba16a0e76d23d4ecdc0
Coder23848 <103940315+Coder23848@users.noreply.github.com>
2024-11-08 20:58:14 -0500

    Hopefully fix screen resolution bug?

--- src/plugin/Plugin.cs
+++ src/plugin/Plugin.cs
@@ -16,15 +16,41 @@ namespace NumberFixes
             On.HUD.Map.ClearSprites += Map_ClearSprites;
             On.RainWorldGame.ShutDownProcess += RainWorldGame_ShutDownProcess;
             On.Menu.ModdingMenu.ShutDownProcess += ModdingMenu_ShutDownProcess;
+
+            Application.focusChanged += Application_focusChanged;
         }
 
+        // screen resolution bug
+        private void Application_focusChanged(bool obj)
+        {
+            if (obj)
+            {
+                FixScreenBug(RWCustom.Custom.rainWorld);
+            }
+        }
+        private void FixScreenBug(RainWorld rainWorld)
+        {
+            if (rainWorld != null && rainWorld.options != null)
+            {
+                Debug.Log("[Number Fixes] Attempting to fix screen bug...");
+                Screen.SetResolution((int)rainWorld.options.ScreenSize.x, (int)rainWorld.options.ScreenSize.y, false);
+                Screen.fullScreen = rainWorld.options.fullScreen;
+                Futile.instance.UpdateScreenWidth((int)rainWorld.options.ScreenSize.x);
+            }
+            else
+            {
+                Debug.Log("[Number Fixes] Unable to fix screen bug, cannot find screen resolution settings.");
+            }
+        }
 
+        // map reveal texture memory leak
         private void Map_ClearSprites(On.HUD.Map.orig_ClearSprites orig, HUD.Map self)
         {
             orig(self);
             Debug.Log("[Number Fixes] Clearing map reveal texture...");
             self.DestroyTextures();
         }
+        // RoomCamera textures memory leak
         private void RainWorldGame_ShutDownProcess(On.RainWorldGame.orig_ShutDownProcess orig, RainWorldGame self)
         {
             orig(self);
@@ -63,6 +89,7 @@ namespace NumberFixes
                 }
             }
         }
+        // Remix thumbnails memory leak
         private void ModdingMenu_ShutDownProcess(On.Menu.ModdingMenu.orig_ShutDownProcess orig, Menu.ModdingMenu self)
         {
             Debug.Log("[Number Fixes] Clearing leakable mod thumbnails...");
@@ -73,6 +100,7 @@ namespace NumberFixes
             orig(self);
         }
 
+        // shortcut glow effect not displaying with "Show Underwater Shortcuts" enabled
         private void ShortcutGraphics_GenerateSprites(On.ShortcutGraphics.orig_GenerateSprites orig, ShortcutGraphics self)
         {
             orig(self);
